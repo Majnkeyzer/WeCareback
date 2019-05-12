c177a23145b3f10ff2986c662b9ec505fb1c15db
Majnkeyzer <43861087+Majnkeyzer@users.noreply.github.com>
2019-05-12 17:05:26 +0200

    Delete ReserveringRepository.java

--- src/main/java/bios/springframework/spring5webapp/repositories/ReserveringRepository.java
+++
@@ -1,9 +0,0 @@
-package bios.springframework.spring5webapp.repositories;
-
-import bios.springframework.spring5webapp.model.Reservering;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ReserveringRepository extends JpaRepository<Reservering, Long> {
-}
