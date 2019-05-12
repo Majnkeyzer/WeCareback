6650edadce445ef5549e1f364770f0d0bff650da
Majnkeyzer <43861087+Majnkeyzer@users.noreply.github.com>
2019-05-12 17:05:20 +0200

    Delete KwRepository.java

--- src/main/java/bios/springframework/spring5webapp/repositories/KwRepository.java
+++
@@ -1,9 +0,0 @@
-package bios.springframework.spring5webapp.repositories;
-
-import bios.springframework.spring5webapp.model.Kijkwijzer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface KwRepository extends JpaRepository<Kijkwijzer,Long> {
-}
