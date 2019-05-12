a3316535d2e18a11abbc05697e8b13fde4c3ea72
Majnkeyzer <43861087+Majnkeyzer@users.noreply.github.com>
2019-05-12 17:05:53 +0200

    Delete ZaalRepository.java

--- src/main/java/bios/springframework/spring5webapp/repositories/ZaalRepository.java
+++
@@ -1,9 +0,0 @@
-package bios.springframework.spring5webapp.repositories;
-
-import bios.springframework.spring5webapp.model.Zaal;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ZaalRepository extends JpaRepository<Zaal, Long> {
-}
