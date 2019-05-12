5dbc77656dc5d2cd3010da03493922a56544008c
Majnkeyzer <43861087+Majnkeyzer@users.noreply.github.com>
2019-05-12 17:01:29 +0200

    Delete SpringSecurityInitializer.java

--- src/main/java/bios/springframework/spring5webapp/config/SpringSecurityInitializer.java
+++
@@ -1,6 +0,0 @@
-package bios.springframework.spring5webapp.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
-}
