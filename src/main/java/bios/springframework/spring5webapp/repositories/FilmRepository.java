f894a3c89049481154fa2637e73569cf4e6d65ed
Majnkeyzer <43861087+Majnkeyzer@users.noreply.github.com>
2019-05-12 17:05:13 +0200

    Delete FilmRepository.java

--- src/main/java/bios/springframework/spring5webapp/repositories/FilmRepository.java
+++
@@ -1,9 +0,0 @@
-package bios.springframework.spring5webapp.repositories;
-
-import bios.springframework.spring5webapp.model.Film;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FilmRepository extends JpaRepository<Film,Long> {
-}
